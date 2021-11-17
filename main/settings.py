decc4d207c49e6b102a2733046d68c998b61cc31
unknown <kimitawanjohi7923@gmail.com>
2021-11-17 16:16:26 +0300

    now this is it

--- main/settings.py
+++ main/settings.py
@@ -67,7 +67,9 @@ ROOT_URLCONF = 'main.urls'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "http://localhost:3000"
+
 ]
 
 AUTHENTICATION_BACKENDS = [
