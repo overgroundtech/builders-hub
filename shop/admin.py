a004d449c82a00500de262b63ccabcb3b9fb6878
kimita <kimita@localhost.localdomain>
2022-02-16 13:52:12 +0300

    made changes to the admin page

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,42 @@
 from django.contrib import admin
 from .models import Category, Product, ProductImage, Order, OrderItem
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(ProductImage)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+admin.site.site_header = "Builders Hub Admin"
+admin.site.site_title = "Builders Hub Admin Portal"
+admin.site.index_title = "Welcome to Builders Hub Admin Portal"
+
+
+class ProductImageAdmin(admin.TabularInline):
+    model = ProductImage
+    extra = 0
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'in_stock')
+    list_filter = ('category', )
+    inlines = [ProductImageAdmin]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+class OrderItemAdmin(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('product', 'quantity', 'unit_price', 'total_price')
+    extra = 0
+
+    def has_add_permission(self, request, obj):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('made_on', 'customer', 'status', 'paid', 'payment')
+    ordering = ('made_on', 'paid',)
+    inlines = [OrderItemAdmin]
